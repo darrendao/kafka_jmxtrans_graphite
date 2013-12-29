f90fb6e53a39c0788b16895ca9d5629c95b83f5c
Ubuntu <ubuntu@ip-10-0-0-227.us-west-1.compute.internal>
2013-12-29 08:01:27 +0000

    first try

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['kafka']['jmx_port'] = 9999
+default['graphite']['host'] = "10.0.0.201"
+default['graphite']['port'] = 2003
---
+++ recipes/default.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: kafka_jmxtrans_graphite
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+service "jmxtrans" do
+  supports :restart => true, :status => true, :reload => true
+  action [ :enable, :start]
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/jmxtrans.rpm" do
+  source "https://github.com/downloads/jmxtrans/jmxtrans/jmxtrans-20121016.145842.6a28c97fbb-0.noarch.rpm"
+  action :create
+end
+
+rpm_package "jmxtrans" do
+    source "#{Chef::Config[:file_cache_path]}/jmxtrans.rpm"
+    action :install
+end
+
+template "/var/lib/jmxtrans/kafka.json" do
+  source "kafka.json.erb"
+  owner node['jmxtrans']['user']
+  group node['jmxtrans']['user']
+  mode  "0755"
+  notifies :restart, "service[jmxtrans]"
+end
+
+template "/etc/sysconfig/jmxtrans" do
+  source "jmxtrans.conf.erb"
+  owner node['jmxtrans']['user']
+  group node['jmxtrans']['user']
+  mode  "0644"
+  notifies :restart, "service[jmxtrans]"
+end
